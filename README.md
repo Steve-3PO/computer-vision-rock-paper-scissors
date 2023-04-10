d260af1e9af7414327993b4d618d18cd68edd5ae
Stephen Delves <stephen.delves@hotmail.co.uk>
2023-04-10 20:05:26 +0100

    rps hard coded, ready to be implemented with model

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 ### Milestone 2
 > The model was trained using over 1000+ pictures for each of the possible inpputs: Rock, Paper, Scissors and Nothing, for when there is a lack of input from the user. The model was then downloaded and imported into this repository to begin accessing it.
 
+### Mileston 3
+> Ignoring the model for a second, basic code was created to play a rock paper scissors game without imaging, so that the model can be put into. The user can provide an input and the computer will make a choice, the results are compared and a winner is announced.
+
---
+++ manual_rps.py
@@ -0,0 +1,34 @@
+import random
+options = ["rock", "paper", "scissors"]
+
+def get_computer_choice():
+    computer_selected = random.choice(options)
+    return computer_selected
+    
+def get_user_choice():
+    while True:
+        user_selected = input("Input: ").lower()
+        if user_selected not in options:
+            print("invalid input")
+        else:
+            break
+    return user_selected
+
+def get_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("It is a tie!")
+    elif user_choice == "rock" and computer_choice == "scissors":
+        print("You won!")
+    elif user_choice == "paper" and computer_choice == "rock":
+        print("You won!")
+    elif user_choice == "scissors" and computer_choice == "paper":
+        print("You won!")
+    else:
+        print("You lost")
+   
+def play():
+    computer_selected = get_computer_choice()
+    user_selected = get_user_choice()
+    get_winner(user_selected, computer_selected)
+    
+play()
